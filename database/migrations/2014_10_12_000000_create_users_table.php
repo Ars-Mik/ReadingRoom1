c9a9e1d6855a3e37fd0f677af3b3e053491307f8
egor <egor3409@gmail.com>
2023-05-30 21:01:15 +0300

    fix users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
+            $table->string('name');
             $table->string('second_name');
             $table->string('third_name');
             $table->string('email')->unique();
